f113abca153e96641a58eb035e0bdfeef46577ed
Park seng chul <ochestra365@naver.com>
2021-08-16 17:10:04 +0900

    초기 커밋.

---
+++ Selection_Sort.c
@@ -0,0 +1,21 @@
+/*/가장 작은 것을 선택해서 제일 앞으로 보내면 어떨까?
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+int main(void) {
+	int i, j, min, index, temp;
+	int array[10] = { 1,10,5,8,7,6,4,3,2,9 };
+	for (i = 0; i < 10; i++) {
+		min = 9999;
+		for (j = i; j < 10; j++) {
+			if (min > array[j])min = array[j]; index = j;
+		}
+		temp = array[i];
+		array[i] = array[index];
+		array[index] = temp;
+	}
+	for (i = 0; i < 10; i++) {
+		printf("%d ", array[i]);
+	}
+	return 0;
+}
+*/
