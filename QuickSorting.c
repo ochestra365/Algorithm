e515acd5bbb4a79fdb42628a257d73ce8d9b0553
Park seng chul <ochestra365@naver.com>
2021-08-17 08:32:53 +0900

    알고리즘커밋

---
+++ QuickSorting.c
@@ -0,0 +1,39 @@
+/*/ 특정한 값을 기준으로 큰 숫자와 작은 숫자를 나누면 어떨까?
+// 겁나 빠른 퀵소트(재귀정렬)
+#define _CRT_SECURE_NO_WARNIGNS
+#include <stdio.h>
+
+int number = 10;
+int data[] = { 1,10,5,8,7,6,4,3,2,9 };
+void show() {
+	int i;
+	for (i = 0; i < number; i++)printf("%d", data[i]);
+}
+void quickSort(int* data, int start, int end) {
+	if (start >= end)return;
+	int key = start;
+	int i = start + 1, j = end, temp;
+	while (i <= j) {
+		while (i <= end && data[i] <= data[key])i++;
+		while (j > start && data[j] >= data[key])j--;
+		if (i > j) {
+			temp = data[j];
+			data[j] = data[key];
+			data[key] = temp;
+		}
+		else {
+			temp = data[i];
+			data[i] = data[j];
+			data[j] = temp;
+		}
+	}
+	quickSort(data, start, j - 1);
+	quickSort(data, j + 1, end);
+}
+
+int main(void) {
+	quickSort(data, 0, number - 1);
+	show();
+	return 0;
+}
+*/
---
+++ testCaseMillion.cpp
@@ -0,0 +1,11 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>	
+#include <algorithm>
+int number, data[1000000];
+int main(void) {
+	scanf("%d", &number);
+	for (int i = 0; i < number; i++)scanf("%d", &data[i]);
+	std::sort(data, data + number); 
+	for(int i=0;i<number;i++){ printf("%d\n", data[i]); }
+	return 0;
+}
