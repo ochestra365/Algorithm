a6c897316bb5794c76caff60dfdde216df4b68b3
ochestra365 <77951823+ochestra365@users.noreply.github.com>
2021-12-03 15:50:10 +0900

    Update Insertion Sort.c

--- Insertion Sort.c
+++ Insertion Sort.c
@@ -1,4 +1,4 @@
-/*#define _CRT_SECURE_NO_WARNINGS
+#define _CRT_SECURE_NO_WARNINGS
 #include <stdio.h>
 
 int main(void) {
@@ -17,4 +17,4 @@ int main(void) {
 		printf("%d", array[i]);
 	}
 	return 0;
-}*/
+}
